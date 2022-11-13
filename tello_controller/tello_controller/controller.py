784e48e77aa5a36a59436a5672bc4ee50ba06b9e
Cezary Wawrzyniak <cezary.waw99@gmail.com>
2022-11-13 16:30:54 +0000

    controller node changes with added xyz aruco following, new world

--- tello_controller/tello_controller/controller.py
+++ tello_controller/tello_controller/controller.py
@@ -12,64 +12,53 @@ from rclpy.node import Node
 
 from std_msgs.msg import Empty
 
+from math import sqrt
+
 
 class ControllerNode(Node):
 
-    class TelloState(Enum):
-        LANDED = 1
-        TAKINGOFF = 2
-        HOVERING = 3
-        FLYING = 4
-        LANDING = 5
-        NONE = 0
-
-    state = TelloState.LANDED
-    next_state = TelloState.NONE
+    state = 0
     action_done = False
+    pose = Pose()
+    follow_marker = 6
 
     def __init__(self):
         super().__init__('controller_node')
         self.tello_controller = self.create_subscription(Empty, '/iisrl/tello_controller', self.main_callback, 10)
-        self.tello_response = self.create_subscription(TelloResponse, '/drone1/tello_response', self.tello_response_callback, 10)
         self.aruco_sub = self.create_subscription(ArucoMarkers, '/aruco_markers', self.get_markers, 10)
 
-        self.tello_service_server = self.create_service(TelloState, '/iisrl/tello_state', self.state_callback)
         self.tello_service_client = self.create_client(TelloAction, '/drone1/tello_action')
         self.service_request = TelloAction.Request()
 
-        self.pose = Pose()
 
-    def state_callback(self, request, response):
-        response.state = str(self.state)
-        response.value = int(self.state.value)
-
-        return response
 
     def main_callback(self, msg):
-        self.get_logger().info("Uruchomiono wezeł")
-        self.action_done = False # False, gdy istnieje misja do wykonania; True, gdy testujemy start i ladowanie
+        self.action_done = False 
 
-        if self.state is not self.TelloState.HOVERING:  
-            self.state = self.TelloState.TAKINGOFF
-            if self.state == self.TelloState.TAKINGOFF:
-                self.taking_off_func()
+        if self.state == 0:
+            print("TAKING OFF")
+            self.get_logger().info("Uruchomiono wezeł")
+            self.take_off()
 
-    def tello_response_callback(self, msg):
-        if msg.rc == 1:
-            self.state = self.next_state
-            self.next_state = self.TelloState.NONE
+        elif self.state == 1:
+            print("FLYING")
+            self.fly_mission()
+
+        elif self.state == 2:
+            print("LANDING")
+            self.land()
 
     def get_markers(self, msg):
         self.marker_list = msg.marker_ids
         self.poses_list = msg.poses 
         # print("DEJ MARKER")
-        if 0 in self.marker_list:
-            id = self.marker_list.index(0)
+        if self.follow_marker in self.marker_list:
+            id = self.marker_list.index(self.follow_marker)
             self.pose = self.poses_list[id]
             # print(self.pose.position)
         
-        if self.state == self.TelloState.HOVERING and not self.action_done:
-            self.mission_func()
+        if self.state == 1 and not self.action_done:
+            self.fly_mission()
 
     def take_off(self):
         # start drona
@@ -78,45 +67,47 @@ class ControllerNode(Node):
 
         self.service_request.cmd = 'takeoff'
         self.tello_service_client.call_async(self.service_request)
+        self.state = 1
         time.sleep(3)
-        self.state = self.TelloState.HOVERING
 
     def land(self):
-        self.state = self.TelloState.LANDING
-
         # ladowanie drona
         while not self.tello_service_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("Oczekuje na dostepnosc uslugi Tello...")
 
         self.service_request.cmd = 'land'
         self.tello_service_client.call_async(self.service_request)   
+        self.state = 0
+        time.sleep(3)
 
     def fly_mission(self):
+        self.state = 1
         while not self.tello_service_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("Oczekuje na dostepnosc uslugi Tello...")
 
-        print("DUPA")
-        print(self.pose.position.z)
-        if self.pose.position.z > 0.4:
+        distance = sqrt(self.pose.position.x * self.pose.position.x + self.pose.position.y * self.pose.position.y + self.pose.position.z * self.pose.position.z)
+        print(distance)
+
+        if self.pose.position.z > 0.15:
             x_vel = 0.1
-        elif self.pose.position.z < - 0.4:
+        elif self.pose.position.z < - 0.15:
             x_vel = -0.1
         else:
             x_vel = 0.0
 
-        if self.pose.position.y > 0.3:
-            y_vel = 0.0
-        elif self.pose.position.y < - 0.3:
-            y_vel = -0.0
+        if self.pose.position.y > 0.05:
+            z_vel = -0.05
+        elif self.pose.position.y < - 0.05:
+            z_vel = 0.05
         else:
-            y_vel = 0.0
+            z_vel = 0.0
 
-        if self.pose.position.x > 0.3:
-            z_vel = 0.0
-        elif self.pose.position.x < - 0.3:
-            z_vel = -0.0
+        if self.pose.position.x > 0.05:
+            y_vel = -0.05
+        elif self.pose.position.x < - 0.05:
+            y_vel = 0.05
         else:
-            z_vel = 0.0
+            y_vel = 0.0
 
 
         print(f'rc {x_vel} {y_vel} {z_vel} 0')
@@ -127,12 +118,11 @@ class ControllerNode(Node):
         if self.pose.position.z < 0.3:
             self.action_done = True
 
-        self.action_done = False
         if self.action_done:
+            self.state = 2
             self.service_request.cmd = 'rc 0 0 0 0'
             self.tello_service_client.call_async(self.service_request)
             print("SKONCZONE")
-            # landing_func()
 
 
         
